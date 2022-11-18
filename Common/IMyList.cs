e73ca25c0aaa33d821c8905703345ea3bf5f0e31
KGahan <kimgahan05@gmail.com>
2022-11-18 09:05:04 +0100

    Factory Pattern added

---
+++ Common/IMyList.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public interface IMyList
+    {
+        void SwitchNodes(Node argFirstNode, Node argSecondNode);
+        void SetSortStrategy(SortStrategy sortStrategy);
+        Node First();
+        void Sort();
+    }
+
+    public abstract class SortStrategy
+    {
+        public abstract void Sort(IMyList list);
+    }
+
+    public class InsertionSort : SortStrategy
+    {
+        public override void Sort(IMyList list)
+        {
+            var nextNode = list.First().next;
+
+            while (nextNode != null)
+            {
+                for (var cur = list.First(); cur.next != null; cur = cur.next)
+                {
+                    if (cur == nextNode)
+                        break;
+                    if ((int)cur.data < (int)nextNode.data)
+                        continue;
+                    (cur.data, nextNode.data) = (nextNode.data, cur.data);
+                }
+                nextNode = nextNode.next;
+            }
+            Console.WriteLine("InsertionSorted list ");
+        }
+    }
+
+    public class InsertionSortReverted : SortStrategy
+    {
+        public override void Sort(IMyList list)
+        {
+            var nextNode = list.First().next;
+
+            while (nextNode != null)
+            {
+                for (var cur = list.First(); cur.next != null; cur = cur.next)
+                {
+                    if (cur == nextNode)
+                        break;
+                    if ((int)cur.data > (int)nextNode.data)
+                        continue;
+                    (cur.data, nextNode.data) = (nextNode.data, cur.data);
+                }
+                nextNode = nextNode.next;
+            }
+            Console.WriteLine("InsertionSorted list ");
+        }
+    }
+}
