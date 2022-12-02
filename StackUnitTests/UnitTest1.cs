917d7aa4da3ae042842c6dbbdcc6fc415fddb509
KGahan <kimgahan05@gmail.com>
2022-12-02 10:38:30 +0100

    Stack added, StackUnitTests added

---
+++ StackUnitTests/UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using CustomStack;
+
+namespace StackUnitTests
+{
+    public class StackUnitTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Push_ThreeElements_StackOrderOK()
+        {
+            MyStack stack = new MyStack();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            Assert.AreEqual(stack.ToString(), "| 1 | 2 | 3 |");
+        }
+
+        [Test]
+        public void PushAndPop_PushThreeTimesPoPonce_TwoElementsInStackLastOneRemoved()
+        {
+            Stack stack = new Stack();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            var last = stack.Pop();
+            Assert.AreEqual(stack.Pop().data, 3);
+            Assert.AreEqual(stack.ToString(), "| 1 | 2 |");
+        }
+    }
+}
