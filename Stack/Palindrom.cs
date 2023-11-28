5ece6c39f0819340bd144f4d4373414f0c1676de
KGahan <kimgahan05@gmail.com>
2023-11-28 12:32:02 +0100

    Programmierübung Palindrom

---
+++ Stack/Palindrom.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    public class Palindrom
+    {
+        Stack<char> stack = new Stack<char>();
+
+        public Palindrom (Stack<char> _stack)
+        {
+            stack = _stack;
+        }
+
+        public bool checkPalindrom(String word)
+        {
+            char[] characters = word.ToCharArray();
+
+            foreach (char character in characters)
+            {
+                stack.Push(character);
+            }
+
+            for (int i = 0; i < characters.Length; i++)
+            {
+                char poppedChar = stack.Peek();
+                stack.Pop();
+
+                if (poppedChar != characters[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
